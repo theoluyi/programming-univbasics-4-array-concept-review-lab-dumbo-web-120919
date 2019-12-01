f6cf945933f5b736bdd5c1bd4cec16a533031047
Theo Carney <theoluyi@gmail.com>
2019-12-01 16:44:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,3 @@
-# def find_element_index(array, value_to_find) 
-#   array.index(value_to_find) 
-#   end
 
 def find_element_index(array, value_to_find)
   (0...array.length).each do |i|
@@ -65,3 +62,35 @@ end
 #     end 
 #    nil 
 # end
+
+#ATTEMPT 4 (ZESHAN)
+# def find_element_index(array, value_to_find) 
+#   array.index(value_to_find) 
+#   end
+
+
+# def find_max_value(array)
+#   i=0 
+#   try = array[0]
+  
+#   loop do 
+    
+#     if try < array[i]
+#       try = array[i]
+#       i+=1
+#     end
+    
+#     if i>array.size 
+#         break
+#     end 
+    
+#   end 
+    
+#     return try 
+# end
+
+  # example array [2, 5, 3, 13, 1]
+  # try = array[0]
+  # if try < array[i]
+    # try = array[i]
+    
